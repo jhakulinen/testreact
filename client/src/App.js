6c05f3d82db0153d924faeb69ffc3998fe9b5c8e
jhakulinen <hakulisenjukka96@gmail.com>
2021-04-27 13:29:04 +0300

    Project 4

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+
+const MusicArray = (props) => {
+  const { data } = props;
+
+  return (
+    <div>
+      <ul>
+        {data.map((item) =>(
+          <li>
+            {item.artist} ( {item.title} )
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const GetAll = () => {
+  const [musics, setMusics] = useState(null)
+  useEffect(() => {
+    fetch("https://mongo-hakulinen.herokuapp.com/api/getall")
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        console.log(data.musics)
+        setMusics(data.musics)
+      })
+  }, [])
+
+  return (
+    <>
+      {
+        musics ?
+          <MusicArray musics={musics}/>
+          : <div>Nothing here. Fetching data...</div>
+      }
+    </>
+  );
+}
+
+
+const App = () => {
+  return(
+    <>
+      <GetAll />
+    </>
+  )
+}
+export default App;
