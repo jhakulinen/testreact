bf63649321dc391a8b16bb54444eb4b17b267504
jhakulinen <hakulisenjukka96@gmail.com>
2021-04-27 22:45:48 +0300

    k

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,98 @@
 //import axios from 'axios';
 import React, {useState, useEffect} from 'react';
 
-const MusicArray = (props) => {
-  const { data } = props;
+// const MusicArray = (props) => {
+//   const { data } = props;
+//   var i = 0;
+//   return (
+//     <div>
+//       <ul>
+//         {data.map((item) => (
+//           <li key={i++}>
+//             {item.artist} ( {item.title} )
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
-  return (
-    <div>
-      <ul>
-        {data.map((item) =>(
-          <li>
-            {item.artist} ( {item.title} )
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+// const GetAll = () => {
+//   const [musics, setMusics] = useState(null)
+//   useEffect(() => {
+//     fetch("https://mongo-hakulinen.herokuapp.com/api/getall")
+//       .then((results) => {
+//         //console.log(results.json());
+//         return results.json();
+        
+//       })
+//       .then((data) => {
+//         console.log(data)
+//         setMusics(data)
+//       })
+//   }, [])
 
-const GetAll = () => {
-  const [musics, setMusics] = useState([])
+//   return (
+//     <>
+//       {
+//         musics ?
+//           <MusicArray musics={musics}/>
+//           : <div>Nothing here. Fetching data...</div>
+//       }
+//     </>
+//   );
+// }
+
+const GetData = () => {
+  const [quotes, setQuotes] = useState(null)
   useEffect(() => {
+   // fetch("https://api.jsonbin.io/b/5e9ef7272940c704e1dc1099%22)
     fetch("https://mongo-hakulinen.herokuapp.com/api/getall")
       .then((results) => {
-        //console.log(results.json());
         return results.json();
-        
       })
-      .then(data => {
-        console.log(data.musics)
-        setMusics(data.musics)
+      .then((data) => {
+        //console.log(data.quotes)
+        console.log(data)
+        //setQuotes(data.quotes)
+        setQuotes(data)
       })
   }, [])
 
   return (
-    <>
+    // Tässä käytetään ns. fragmentteja, eli palautetaan tulosjoukko ilman ympäröivää DIV-elementtiä
+    <> 
       {
-        musics ?
-          <MusicArray musics={musics}/>
-          : <div>Nothing here. Fetching data...</div>
+        quotes ?
+          <SitaattiTaulu data={quotes} />
+          : <div>Nothing here.Fething data...</div>
       }
-    </>
+    </> // fragmenttien lopetustägi
   );
 }
+
+
+const SitaattiTaulu = (props)=>{
+  const {data}=props;
+  var i = 0;
+  return (
+    <div>
+      <ul>
+        {data.map((item)=>(
+        <li key={i++}>
+          {item.title} ({item.artist})
+        </li>
+        ))}
+      </ul>
+    </div>
+  )
+};
 
 
 const App = () => {
   return(
     <>
-      <GetAll />
+      <GetData />
     </>
   )
 }
